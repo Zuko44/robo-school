d91be53bf0948deb8141af4b224171152910a2b5
Vladislav Kuryanov <vkuryanov@marfa-tech.com>
2025-06-05 16:42:40 +0300

    BLOCK3-4-feedback-form

---
+++ src/components/input/input.tsx
@@ -0,0 +1,88 @@
+import { InputHTMLAttributes, DetailedHTMLProps, ChangeEvent } from 'react';
+import { UseFormRegister, FieldValues, Path } from 'react-hook-form';
+import styles from './input.module.scss';
+
+type CommonProps = {
+  label?: string;
+  error?: string;
+  additionalClassname?: string;
+};
+
+type ControlledProps = {
+  register?: never;
+  name?: string;
+  value: string;
+  onChange: (value: string) => void;
+};
+
+type UncontrolledProps<T extends FieldValues> = {
+  register: UseFormRegister<T>;
+  name: Path<T>;
+  value?: never;
+  onChange?: never;
+};
+
+type NativeInputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+export type InputProps<T extends FieldValues> = (ControlledProps | UncontrolledProps<T>) &
+  CommonProps &
+  Omit<NativeInputProps, 'name' | 'value' | 'onChange'>;
+
+export function Input<T extends FieldValues>({
+  label,
+  error,
+  additionalClassname,
+  register,
+  name,
+  value,
+  onChange,
+  placeholder,
+  ...rest
+}: InputProps<T>) {
+  const isControlled = Boolean(onChange);
+
+  const handleInputValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!onChange) {
+      return;
+    }
+
+    onChange(event.target.value);
+  };
+
+  const inputRegistration = register && name ? register(name) : {};
+  const dynamicPropsObject = isControlled
+    ? { value, onChange: handleInputValueChange }
+    : inputRegistration;
+
+  const buildInputClassname = () => {
+    let resultClassname = styles.input;
+
+    if (additionalClassname) {
+      resultClassname = `${resultClassname} ${additionalClassname}`;
+    }
+
+    if (error) {
+      resultClassname = `${resultClassname} ${styles.inputError}`;
+    }
+
+    return resultClassname;
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor={name} className={styles.visuallyHidden}>
+        {label}
+      </label>
+
+      {Boolean(error) && <span className={styles.error}>{error}</span>}
+
+      <input
+        id={name}
+        name={name}
+        className={buildInputClassname()}
+        {...dynamicPropsObject}
+        {...rest}
+      />
+    </div>
+  );
+}
--- src/modules/main-page-content/components/main-form/main-form.tsx
+++ src/modules/main-page-content/components/main-form/main-form.tsx
@@ -1,16 +1,67 @@
 import { Container } from '@/components/container';
 
 import styles from './main-form.module.scss';
+import { Input } from '@/components/input';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button } from '@/components/button';
+import { formSchema } from '@/helpers/schema';
+
+type FormValues = {
+  name: string;
+  phone: string;
+  email: string;
+};
 
 export const MainForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmit = (data: FormValues) => {
+    console.log('Форма отправлена:', data);
+    reset();
+  };
+
   return (
     <section className={styles.course}>
       <Container>
-        <div className={styles.wrapper}>
-          <div className={styles.text}>
-            <h2 className={styles.title}>Запишитесь на курс со скидкой 10%</h2>
-            <p className={styles.description}>Акция действительна до 10 марта 2022 года</p>
+        <div className={styles.inner}>
+          <div className={styles.wrapper}>
+            <div className={styles.text}>
+              <h2 className={styles.title}>Запишитесь на курс со скидкой 10%</h2>
+              <p className={styles.description}>Акция действительна до 10 марта 2022 года</p>
+            </div>
           </div>
+          <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+            <Input
+              register={register}
+              name="name"
+              placeholder="Введите имя"
+              error={errors.name?.message}
+            />
+            <Input
+              register={register}
+              name="phone"
+              placeholder="Телефон"
+              error={errors.phone?.message}
+            />
+            <Input
+              register={register}
+              name="email"
+              placeholder="E-mail"
+              error={errors.email?.message}
+            />
+
+            <Button type="submit" variant="secondary" additionalClassname={styles.buttonMain}>
+              Оставить заявку
+            </Button>
+          </form>
         </div>
       </Container>
     </section>
