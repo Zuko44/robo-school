d1ef491e9c44e5299ad4f661056e7019d3b36dcd
Vladislav Kuryanov <vkuryanov@marfa-tech.com>
2025-06-10 15:03:27 +0300

    feature: created sliders

---
+++ src/components/slider-navigation/slider-navigation.tsx
@@ -0,0 +1,26 @@
+import { ArrowLeftIcon } from '@/assets/icons/ArrowLeftIcon';
+import { ArrowRightIcon } from '@/assets/icons/ArrowRightIcon';
+import { MutableRefObject } from 'react';
+import styles from './slider-navigation.module.scss';
+
+interface ControlsProps {
+  scrollbarRef: MutableRefObject<HTMLDivElement | null>;
+  onPrev: () => void;
+  onNext: () => void;
+}
+
+export const SliderNavigation = ({ scrollbarRef, onPrev, onNext }: ControlsProps) => {
+  return (
+    <div className={styles.navigations}>
+      <div className={`${styles.scrollbar} swiper-scrollbar`} ref={scrollbarRef} />
+      <div className={styles.navigationsArrows}>
+        <div className={styles.navigationsArrow} onClick={onPrev}>
+          <ArrowLeftIcon />
+        </div>
+        <div className={styles.navigationsArrow} onClick={onNext}>
+          <ArrowRightIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
